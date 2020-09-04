26119182854a0f7ce1e88f613d6e61a42f773fe8
Stiven Avila <stivenavilam@gmail.com>
2020-09-04 11:48:34 -0500

    Code chat

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+ 
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
